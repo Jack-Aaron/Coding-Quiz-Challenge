89e7f6b7c1beed093631cf40d9a878714675f23e
Jonathan Alpart <panjialang@gmail.com>
2020-04-25 18:59:37 -0500

    button clears main screen

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 var startButton = document.querySelector("button");
 var container = document.querySelector(".container");
+var main = document.querySelector("main");
+
+startButton.addEventListener("click", function() {
+    main.innerHTML = "";
+});
