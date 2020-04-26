81fb6701ef3f054df1f8878ba57c3751aa15d7cc
Jonathan Alpart <panjialang@gmail.com>
2020-04-25 22:00:59 -0500

    constructing a function that loads questions

--- script.js
+++ script.js
@@ -35,5 +35,30 @@ function createQuiz() {
     document.body.children[1].appendChild(sectionEl);
     var figureEl = document.createElement("figure");
     document.body.children[1].children[0].appendChild(figureEl);
+    var h1 = document.createElement("h1");
+    document.body.children[1].children[0].children[0].appendChild(h1);
+    var ol = document.createElement("ol");
+    document.body.children[1].children[0].children[0].appendChild(ol);
+
+
+
+    return newQuestion();
+}
+
+function newQuestion() {
+
+    var questions = ["What is 'true'?"];
+    var h1 = document.querySelector("h1");
+    h1.textContent = questions[0];
+    var answer = ["boolean", "string", "variable", "tag"]
+
+    for (let i = 0; i < 4; i++) {
+        var li = document.createElement("li");
+        document.body.children[1].children[0].children[0].children[1].appendChild(li);
+        li.textContent = answer[i];
+
+    }
+
+
 }
 
