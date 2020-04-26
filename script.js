529a9dc643118c59084b0f3d94e4d0bee073b19f
Jonathan Alpart <panjialang@gmail.com>
2020-04-26 16:01:53 -0500

    attempting to automate the population of buttons with question and answer information

--- script.js
+++ script.js
@@ -8,80 +8,88 @@ var secondsEl = document.querySelector("#seconds");
 var score = 0;
 questionNumber = 0;
 
-// this controls the start button
-startButton.addEventListener("click", function () {
-    main.innerHTML = "";
-    startQuiz();
-});
+// put all questions into array
 
+function writeQuestions() {
 
-
-// this is the amount of time for player
-var secondsLeft = 75;
-
-function startQuiz() {
-    startTimer();
-    createQuiz();
+    var questions = ["The value \"true\" is an example of what type of variable?", "Which of the following methods could be called to change a font-size of the content of an element?", "A user will be prompted if Javascript code runs which of the following methods:"];
 }
 
-function startTimer() {
-    secondsEl.textContent = secondsLeft;
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
+
+
+
+    // this controls the start button
+    startButton.addEventListener("click", function () {
+        main.innerHTML = "";
+        startQuiz();
+    });
+
+    // this is the amount of time for player
+    var secondsLeft = 75;
+
+    function startQuiz() {
+        startTimer();
+        createQuiz();
+    }
+
+    // simple interval for Timer
+    function startTimer() {
         secondsEl.textContent = secondsLeft;
+        var timerInterval = setInterval(function () {
+            secondsLeft--;
+            secondsEl.textContent = secondsLeft;
 
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                //sendMessage();
+            }
 
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            //sendMessage();
-        }
+        }, 1000);
+        return secondsLeft;
+    }
 
-    }, 1000);
-    return secondsLeft;
-}
+    // this function creates the structure of the page controlling the quiz
+    function createQuiz() {
+        var sectionEl = document.createElement("section");
+        document.body.children[1].appendChild(sectionEl);
+        sectionEl.setAttribute("class", "container");
+        var figureEl = document.createElement("figure");
+        document.body.children[1].children[0].appendChild(figureEl);
+        figureEl.setAttribute("class", "container");
+        var h1 = document.createElement("h1");
+        document.body.children[1].children[0].children[0].appendChild(h1);
+        var ol = document.createElement("ol");
+        document.body.children[1].children[0].children[0].appendChild(ol);
+        ol.setAttribute("style", "padding-top:4em;");
 
-// this function creates the structure of the page controlling the quiz
-function createQuiz() {
-    var sectionEl = document.createElement("section");
-    document.body.children[1].appendChild(sectionEl);
-    sectionEl.setAttribute("class", "container");
-    var figureEl = document.createElement("figure");
-    document.body.children[1].children[0].appendChild(figureEl);
-    figureEl.setAttribute("class", "container");
-    var h1 = document.createElement("h1");
-    document.body.children[1].children[0].children[0].appendChild(h1);
-    var ol = document.createElement("ol");
-    document.body.children[1].children[0].children[0].appendChild(ol);
-    ol.setAttribute("style", "padding-top:4em;");
-
-    return newQuestion();
-}
-
-
-
-// this function feeds the information of each question into the page structure
-function newQuestion() {
-
- 
-
-    var questions = ["What is 'true'?"];
-    var h1 = document.querySelector("h1");
-    h1.textContent = questions[questionNumber];
-    var answer = ["boolean", "string", "variable", "tag"]
-    questionNumber++;
-
-    for (let i = 0; i < 4; i++) {
-        var li = document.createElement("li");
-        document.body.children[1].children[0].children[0].children[1].appendChild(li);
-        var answerButton = document.createElement("button")
-        document.body.children[1].children[0].children[0].children[1].children[i].appendChild(button);
-        answerButton.setAttribute("style", "padding:1em;");
-        answerButton.textContent = answer[i];
-
+        // not sure if this works...
+        return newQuestion();
     }
 
 
-}
+
+    // this function feeds the information of each question into the page structure
+    function newQuestion() {
+
+        var h1 = document.querySelector("h1");
+        h1.textContent = questions[questionNumber];
+        var answerButton = document.querySelector("button");
+        button.textContent = answer[questionNumber];
+        
+        questionNumber++;
+
+        for (let i = 0; i < 4; i++) {
+            var li = document.createElement("li");
+            document.body.children[1].children[0].children[0].children[1].appendChild(li);
+            var answerButton = document.createElement("button")
+            document.body.children[1].children[0].children[0].children[1].children[i].appendChild(button);
+            answerButton.setAttribute("style", "padding:1em;");
+            answerButton.textContent = answer[i];
+
+        }
+
+
+    }
 
 /* choosing an answer
 answerButton.addEventListener("click", function () {
