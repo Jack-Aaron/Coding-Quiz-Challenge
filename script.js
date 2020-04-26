582dba63dc80f5dc99df26f60e4438efafecffb7
Jonathan Alpart <panjialang@gmail.com>
2020-04-25 19:25:39 -0500

    adding timer

--- script.js
+++ script.js
@@ -4,4 +4,28 @@ var main = document.querySelector("main");
 
 startButton.addEventListener("click", function() {
     main.innerHTML = "";
+    // startQuiz();
 });
+
+function startTimer() {
+  
+    interval = setInterval(function() {
+      secondsElapsed++;
+      renderTime();
+    }, 1000);
+  }
+
+  function renderTime() {
+    minutesDisplay.textContent = getFormattedMinutes();
+    secondsDisplay.textContent = getFormattedSeconds();
+  
+    if (secondsElapsed >= totalSeconds) {
+      if (status === "Working") {
+        alert("Time for a break!");
+      } else {
+        alert("Time to get back to work!");
+      }
+  
+      stopTimer();
+    }
+  }
