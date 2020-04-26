250d721ef7b36b6d5ef2c2ae3d80800cf957e9a7
Jonathan Alpart <panjialang@gmail.com>
2020-04-26 15:37:53 -0500

    adjusting spacing and positioning of answer buttons

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+// define dynamic parts of the page
 var startButton = document.querySelector("button");
 var container = document.querySelector(".container");
 var main = document.querySelector("main");
 var secondsEl = document.querySelector("#seconds");
 
-
+// this controls the start button
 startButton.addEventListener("click", function () {
     main.innerHTML = "";
     startQuiz();
 });
 
+// this is the amount of time for player
 var secondsLeft = 75;
 
 function startQuiz() {
@@ -32,19 +34,24 @@ function startTimer() {
     return secondsLeft;
 }
 
+// this function creates the structure of the page controlling the quiz
 function createQuiz() {
     var sectionEl = document.createElement("section");
     document.body.children[1].appendChild(sectionEl);
+    sectionEl.setAttribute("class", "container");
     var figureEl = document.createElement("figure");
     document.body.children[1].children[0].appendChild(figureEl);
+    figureEl.setAttribute("class", "container");
     var h1 = document.createElement("h1");
     document.body.children[1].children[0].children[0].appendChild(h1);
     var ol = document.createElement("ol");
     document.body.children[1].children[0].children[0].appendChild(ol);
+    ol.setAttribute("style", "padding-top:4em;");
 
     return newQuestion();
 }
 
+// this function feeds the information of each question into the page structure
 function newQuestion() {
 
     var questions = ["What is 'true'?"];
@@ -57,6 +64,7 @@ function newQuestion() {
         document.body.children[1].children[0].children[0].children[1].appendChild(li);
         var button = document.createElement("button")
         document.body.children[1].children[0].children[0].children[1].children[i].appendChild(button);
+        button.setAttribute("style", "padding:1em;");
         button.textContent = answer[i];
 
     }
